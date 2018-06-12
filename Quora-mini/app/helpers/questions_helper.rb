036d85608515452f2a0e35cb63077563ae9e6dd8
Puneeth Kumar DN <puneethdh@gmail.com>
2018-06-13 01:31:10 +0530

    Add categories for quesions Write helper functions

--- Quora-mini/app/helpers/questions_helper.rb
+++ Quora-mini/app/helpers/questions_helper.rb
@@ -1,2 +1,14 @@
 module QuestionsHelper
+  def options_for_category
+    [
+      ['Health'],
+      ['Ruby on Rails'],
+      ['Business'],
+      ['Social Media'],
+      ['Science'],
+      ['Technology'],
+      ['News'],
+      ['Education']
+    ]
+  end
 end
