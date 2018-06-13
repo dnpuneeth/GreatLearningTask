2dcb8ca996ef1fc1901779b160a85d9d4b78a668
Puneeth Kumar DN <puneethdh@gmail.com>
2018-06-14 02:54:16 +0530

    Complete implementation of Voting support.

--- Quora-mini/app/controllers/votes_controller.rb
+++ Quora-mini/app/controllers/votes_controller.rb
@@ -1,11 +1,17 @@
 class VotesController < ApplicationController
-  before_action :set_votes
+  #before_action :set_votes
+  attr_accessor :vote
+
   def create
-    byebug
     @comment = Comment.find(params[:comment_id])
     @user = User.find(params[:user_id])
     @votes = @comment.votes.build(vote_params)
     @votes.user = @user
+    if @votes.save
+      flash[:success] = 'Up Voted' if vote_params[:vote].to_i.eql? 1
+      flash[:danger] = 'Down Voted' if vote_params[:vote].to_i.eql? -1
+      redirect_to user_question_path(id: @comment.question_id)
+    end
   end
 
   private
